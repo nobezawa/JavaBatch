2e69b804425a1e19395bdcc0ecc14a5ece1764f1
nobezawa <charmy.nobezawa@gmail.com>
2015-07-29 10:28:31 +0900

    Initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+/**
+ * Created by nobesawa on 15/07/29.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
